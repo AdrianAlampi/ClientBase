bcdae95c49dd084ebb848449e9f1ffc1295a1498
adrya <adrya@DESKTOP-038MMD9.homenet.telecomitalia.it>
2021-03-04 17:33:06 +0100

    es1

--- clientBase/src/clientbase/ClientBase.java
+++ clientBase/src/clientbase/ClientBase.java
@@ -5,6 +5,13 @@
  */
 package clientbase;
 
+import java.io.*;
+import static java.lang.Thread.sleep;
+import java.net.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author adrya
@@ -15,7 +22,32 @@ public class ClientBase {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Apertua connessione");
+        try {
+            Socket server = new Socket("127.0.0.1", 5000);
+
+            PrintWriter out
+                    = new PrintWriter(server.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(server.getInputStream()));
+            Scanner tastiera = new Scanner(System.in);
+            String s = "";
+
+            while (!s.equals("exit")) {
+                System.out.println("Inserire frase da inviare al server:");
+                s = tastiera.nextLine();
+                System.out.println("frase inserita: " + s);
+                out.println(s);
+                String risposta = in.readLine();
+                System.out.println("risposta del server: " + risposta);
+            }
+
+            in.close();
+            server.close();
+            System.out.println("chiusura connessione");
+        } catch (IOException ex) {
+            Logger.getLogger(ClientBase.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
 }
